80f72feeb744a95ea82261b683179b70868e0668
RiverOcean <chenjiangyang111@hotmail.com>
2026-05-11 16:11:26 +0800

    V1.2
    1.新增收藏网站；
    2.代码结构优化；
    3.图标更新。

---
+++ script.js
@@ -0,0 +1,12 @@
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+
+function openSidebar() {
+    sidebar.classList.add("active");
+    overlay.classList.add("active");
+}
+
+function closeSidebar() {
+    sidebar.classList.remove("active");
+    overlay.classList.remove("active");
+}
